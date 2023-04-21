da2e65cb3a8b374b592bf3b0d65e5dfbbe1d27a4
Yash Wasalwar <71366827+Yash-Wasalwar-07@users.noreply.github.com>
2023-04-21 08:49:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
-import subprocess
+# import subprocess
 
-packages = str(subprocess.run('pip list', capture_output=True))
-st.markdown(packages.replace('\\r\\n', '  \\\n'))
+# packages = str(subprocess.run('pip list', capture_output=True))
+# st.markdown(packages.replace('\\r\\n', '  \\\n'))
 
 import torch,torchvision
 # import req
